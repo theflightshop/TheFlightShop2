df897951e6b47cf4a0f3d1f0002c3a45d23c42b2
Nick Redmond <nicholas.john.redmond@gmail.com>
2019-08-11 15:13:17 -0600

    initial commit. created MVC Core app and implemented most of the static pages for Flight Shop (excep Contact and Wingtip Rapid Install Kit).

---
+++ TheFlightShop/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheFlightShop.Models;
+
+namespace TheFlightShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        #region Maintenance
+
+        public IActionResult CompanyProfile()
+        {
+            return View();
+        }
+
+        public IActionResult Facility()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Parts()
+        {
+            return View();
+        }
+
+        public IActionResult Modifications()
+        {
+            return View();
+        }
+
+        public IActionResult CabinBaggageDoorStruts()
+        {
+            return View();
+        }
+
+        public IActionResult CabinDoorCables()
+        {
+            return View();
+        }
+
+        public IActionResult WindowShades()
+        {
+            return View();
+        }
+
+        public IActionResult InstrumentPanels()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Click Bond
+
+        public IActionResult ClickBondAuthorizedDistributor()
+        {
+            return View();
+        }
+
+        public IActionResult ClickBondInstallation()
+        {
+            return View();
+        }
+
+        public IActionResult ClickBondStrengthData()
+        {
+            return View();
+        }
+
+        public IActionResult OrderingInformation()
+        {
+            return View();
+        }
+
+        #endregion
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
