d56f900443e4c7c8a73f684ad084549dd62edf8a
Nick Redmond <nicholas.john.redmond@gmail.com>
2019-09-08 12:45:10 -0600

    modified product detail ordering form so unlisted parts can have either part number or description provided

--- TheFlightShop/wwwroot/js/products/product-detail.js
+++ TheFlightShop/wwwroot/js/products/product-detail.js
@@ -7,18 +7,35 @@ function closeInstallationExamples() {
     document.getElementById('flightshop-installation-examples').style.display = 'none';
 }
 
-function addSpecialPart() {
+var unlistedPartCount = 1;
+function addUnlistedPart() {
     var specialPartRow = document.createElement('tr');
 
     var partNumberCol = document.createElement('td');
     partNumberCol.classList.add('flightshop-parts-table-cell');
 
     var partNumberInput = document.createElement('input');
-    partNumberInput.placeholder = 'Special part #';
+    partNumberInput.id = 'part-nr-input-' + unlistedPartCount;
+    partNumberInput.placeholder = 'Part #';
+    partNumberInput.style.maxWidth = '75%';
+    partNumberInput.style.float = 'left';
     partNumberCol.appendChild(partNumberInput);
+    partNumberCol.style.borderRight = 'none';
+
+    var orCol = document.createElement('span');
+    orCol.innerHTML = ' or ';
+    orCol.style.width = '50px';
+    orCol.style.marginLeft = '1em';
+    partNumberCol.appendChild(orCol);
 
     var descriptionCol = document.createElement('td');
     descriptionCol.classList.add('flightshop-parts-table-cell');
+
+    var descriptionInput = document.createElement('input');
+    descriptionInput.id = 'description-input-' + unlistedPartCount;
+    descriptionInput.placeholder = 'Description of part';
+    descriptionCol.appendChild(descriptionInput);
+    descriptionCol.style.borderLeft = 'none';
 
     var priceCol = document.createElement('td');
     priceCol.classList.add('flightshop-parts-table-cell');
@@ -36,8 +53,13 @@ function addSpecialPart() {
     addToCartCol.appendChild(addToCartInput);
 
     var addToCartButton = document.createElement('button');
+    addToCartButton.id = 'button-add-' + unlistedPartCount;
     addToCartButton.type = 'button';
     addToCartButton.innerHTML = 'Add';
+    var inputId = unlistedPartCount;
+    addToCartButton.addEventListener('click', function () {
+        addToCart('' + inputId)
+    });
     addToCartCol.appendChild(addToCartButton);
 
     specialPartRow.appendChild(partNumberCol);
@@ -45,4 +67,19 @@ function addSpecialPart() {
     specialPartRow.appendChild(priceCol);
     specialPartRow.appendChild(addToCartCol);
     document.getElementById('flightshop-parts-table').appendChild(specialPartRow);
+
+    unlistedPartCount++;
+}
+
+function addToCart(inputId) {
+    var partNumber = document.getElementById('part-nr-input-' + inputId).value;
+    var description = document.getElementById('description-input-' + inputId).value;
+    var infoProvided = partNumber || description;
+
+    if (infoProvided) {
+        console.log('add to cart', partNumber, description);
+    }
+
+    var validationAlert = document.getElementById('flightshop-invalid-unlisted-part-alert');
+    validationAlert.style.display = infoProvided ? 'none' : 'block';
 }
