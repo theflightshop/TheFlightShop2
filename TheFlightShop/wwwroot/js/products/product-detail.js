7d7e529187a7adfec9349b63afe8d9bf21a8ffb7
Nick Redmond <nicholas.john.redmond@gmail.com>
2019-09-09 16:51:10 -0600

    add logic to update products-added overlay

--- TheFlightShop/wwwroot/js/products/product-detail.js
+++ TheFlightShop/wwwroot/js/products/product-detail.js
@@ -46,6 +46,7 @@ function addUnlistedPart() {
     addToCartCol.classList.add('flightshop-parts-table-cell');
 
     var addToCartInput = document.createElement('input');
+    addToCartInput.id = 'item-quantity-' + unlistedPartCount;
     addToCartInput.classList.add('flightshop-part-add-to-cart-input');
     addToCartInput.value = '1';
     addToCartInput.type = 'number';
@@ -58,7 +59,7 @@ function addUnlistedPart() {
     addToCartButton.innerHTML = 'Add';
     var inputId = unlistedPartCount;
     addToCartButton.addEventListener('click', function () {
-        addToCart('' + inputId)
+        addUnlistedToCart('' + inputId)
     });
     addToCartCol.appendChild(addToCartButton);
 
@@ -71,15 +72,77 @@ function addUnlistedPart() {
     unlistedPartCount++;
 }
 
-function addToCart(inputId) {
+function addUnlistedToCart(inputId) {
     var partNumber = document.getElementById('part-nr-input-' + inputId).value;
     var description = document.getElementById('description-input-' + inputId).value;
-    var infoProvided = partNumber || description;
-
-    if (infoProvided) {
-        console.log('add to cart', partNumber, description);
-    }
+    var quantity = getQuantity('item-quantity-' + inputId);
 
     var validationAlert = document.getElementById('flightshop-invalid-unlisted-part-alert');
-    validationAlert.style.display = infoProvided ? 'none' : 'block';
+    var quantityAlert = document.getElementById('flightshop-invalid-part-quantity');
+    var infoProvided = partNumber || description;
+
+    if (infoProvided && quantity) {
+        validationAlert.style.display = 'none';
+        quantityAlert.style.display = 'none';
+        requestAddToCart(infoProvided, quantity, null);
+    }
+    else if (infoProvided) {
+        quantityAlert.style.display = 'block';
+    }
+    else {
+         validationAlert.style.display = 'block';
+    }
 }
+
+function addToCart(partNumber, price) {
+    var quantity = getQuantity('addToCartQuantity-' + partNumber);
+    var quantityAlert = document.getElementById('flightshop-invalid-part-quantity');
+
+    if (quantity) {
+        quantityAlert.style.display = 'none';
+        requestAddToCart(partNumber, quantity, price);
+    }
+    else {
+        quantityAlert.style.display = 'block';
+    }
+}
+
+function getQuantity(id) {
+    var value = document.getElementById(id).value;
+    var parsed = parseFloat(value);
+    return parsed > 0 ? parsed : null;
+}
+
+function requestAddToCart(codeOrDescription, quantity, price) {
+    addItemToProductAddedDisplay(codeOrDescription, quantity, price);
+}
+
+function addItemToProductAddedDisplay(codeOrDescription, quantity, price) {
+    var itemAdded = document.createElement('p');
+    itemAdded.classList.add('flightshop-product-added-item-container');
+
+    var itemDescription = document.createElement('em');
+    itemDescription.classList.add('flightshop-product-added-item');
+    itemDescription.innerHTML = quantity + '&times;&nbsp;' + codeOrDescription.slice(0, 14) + '&nbsp;'
+    itemAdded.appendChild(itemDescription);
+
+    var itemPrice = document.createElement('span');
+    itemPrice.classList.add('flightshop-product-added-price');
+    var priceValue = parseFloat(price) || 0;
+    if (priceValue) {
+        var totalCost = priceValue * quantity;
+        itemPrice.innerHTML = '$' + totalCost.toFixed(2);
+    }
+    else {
+        itemPrice.innerHTML = '(quote)';
+    }
+    itemAdded.appendChild(itemPrice);
+
+    document.getElementById('flightshop-product-added-items').appendChild(itemAdded);
+}
+
+function closeProductsAddedOverlay() {
+    document.getElementById('flightshop-added-to-cart-overlay').style.display = 'none';
+    document.getElementById('flightshop-product-added-items').innerHTML = '';
+}
+
